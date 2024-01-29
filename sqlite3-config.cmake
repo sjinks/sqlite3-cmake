28981fe144864c4c7e95abbcb963a7d05c58a3a9
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2024-01-29 03:05:54 +0200

    fix: compatibility with FindSQLite3

--- sqlite3-config.cmake
+++ sqlite3-config.cmake
@@ -2,8 +2,8 @@ get_filename_component(SQLITE3_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 
 list(APPEND CMAKE_MODULE_PATH ${SQLITE3_CMAKE_DIR})
 
-if(NOT TARGET sqlite3:sqlite3)
+if(NOT TARGET SQLite::SQLite3)
     include("${SQLITE3_CMAKE_DIR}/sqlite3-targets.cmake")
 endif()
 
-set(SQLITE3_LIBRARIES sqlite3:sqlite3)
+set(SQLite3_LIBRARIES SQLite::SQLite3)
